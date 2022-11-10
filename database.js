d08c7db2b7ab1dbce098ffd727b1299b545a2f65
Romain HADDAD <romain.haddad@gmail.com>
2022-11-10 11:54:56 +0100

    Express 2 quest

---
+++ database.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const mysql = require("mysql2/promise");
+
+const database = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+database
+  .query("SELECT * FROM movies")
+  .then(([movies]) => {
+    console.log(movies);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+module.exports = database;
